9b6a057d98d075d843fcea6222c57598626533b3
Dennis Fink <dennis.fink@c3l.lu>
2016-12-13 23:50:18 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='petri',
-    version='0.0.1-rc7',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires={
