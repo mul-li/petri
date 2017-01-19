9e8c835d34c5fad631452b140016bfbcefb14ee1
Dennis Fink <dennis.fink@c3l.lu>
2017-01-19 01:18:23 +0100

    Updated version
    --HG--
    branch : feature-celery

--- petri/__init__.py
+++ petri/__init__.py
@@ -5,7 +5,7 @@ import base64
 from flask import Flask
 from flask_qrcode import QRcode
 
-from celery import Celery, Task
+from celery import Celery
 
 import pkg_resources
 
@@ -14,15 +14,6 @@ config_file = os.path.abspath('config.json')
 qrcode = QRcode()
 
 celery = Celery(__name__)
-
-
-class AppContextTask(Task):
-
-    abstract = True
-
-    def __call__(self, *args, **kwargs):
-        with self.app.flask_app.app_context():
-            return super().__call__(self, *args, **kwargs)
 
 
 def create_app():
@@ -40,14 +31,12 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', _default_secret_key)
     app.config.setdefault('PETRI_SITE_TITLE', 'Petri')
     app.config.setdefault('DATABASE', 'petri.pickle')
-    app.config.setdefault('PETRI_CELERY_BROKER_URL', 'pyamqp://localhost:5672/petri')
+    app.config.setdefault('PETRI_CELERY_BROKER_URL', 'pyamqp://petri:petri@localhost:5672/petri')
 
-    celery_config = app.config.get_namespace('PETRI_CELERY_')
-    celery.conf.update(celery_config)
-    celery.Task = AppContextTask
+    celery_config = app.config.get_namespace('PETRI_CELERY_', lowercase=False)
 
-    if not hasattr(celery, 'flask_app'):
-        celery.flask_app = app
+    from mulli import make_celery
+    make_celery(app, celery_config)
 
     from .base36 import Base36Converter
     app.url_map.converters['base36'] = Base36Converter
--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='petri',
-    version='1.0.1',
+    version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires={
         'Flask==0.11.1',
         'Flask-WTF==0.13.1',
         'Flask-QRcode==0.8.0',
-        'mulli==0.0.3',
-        'celery==4.0.2',
+        'mulli==0.0.5',
     },
 )
