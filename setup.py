69367218808e9b4f7c9f3db8f2223675c8222b11
Dennis Fink <dennis.fink@c3l.lu>
2017-06-30 19:59:10 +0200

    Updated version
    --HG--
    branch : mulli-0.0.6

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='petri',
-    version='1.1.2',
+    description='An URL-shortener service, that doesn\'t track you',
+    version='1.1.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires={
         'Flask==0.11.1',
         'Flask-WTF==0.13.1',
         'Flask-QRcode==0.8.0',
-        'mulli==0.0.5',
+        'mulli==0.0.8-dev',
     },
 )
