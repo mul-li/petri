f3479d00b7ccba1ae38ccd7c8d10c9ec501d79a4
Dennis Fink <dennis.fink@c3l.lu>
2017-06-30 19:58:33 +0200

    Update to latest mulli release
    --HG--
    branch : mulli-0.0.6

--- petri/__init__.py
+++ petri/__init__.py
@@ -9,11 +9,17 @@ from celery import Celery
 
 import pkg_resources
 
+from mulli import Mulli
+from mulli import Celery as CeleryExt
+
 config_file = os.path.abspath('config.json')
 
 qrcode = QRcode()
 
 celery = Celery(__name__)
+
+celery_ext = CeleryExt()
+mulli_ext = Mulli()
 
 
 def create_app():
@@ -26,17 +32,18 @@ def create_app():
         pass
 
     qrcode.init_app(app)
+    mulli_ext.init_app(app)
 
     _default_secret_key = base64.b64encode(os.urandom(32)).decode('utf-8')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', _default_secret_key)
-    app.config.setdefault('PETRI_SITE_TITLE', 'Petri')
-    app.config.setdefault('DATABASE', 'petri.pickle')
+
+    app.config['SITE_TITLE'] = app.config.get('PETRI_SITE_TITLE', 'Petri')
+    app.config['ONION_ADDRESS'] = app.config.get('PETRI_ONION_ADDRESS', None)
+    app.config['DATABASE'] = app.config.get('PETRI_DATABASE', 'petri.pickle')
+
     app.config.setdefault('PETRI_CELERY_BROKER_URL', 'pyamqp://petri:petri@localhost:5672/petri')
-
     celery_config = app.config.get_namespace('PETRI_CELERY_', lowercase=False)
-
-    from mulli import make_celery
-    make_celery(app, celery_config)
+    celery_ext.init_app(app, celery_config)
 
     from .base36 import Base36Converter
     app.url_map.converters['base36'] = Base36Converter
@@ -49,7 +56,11 @@ def create_app():
 
     @app.context_processor
     def inject_version():
-        return dict(PETRI_VERSION=pkg_resources.require('petri')[0].version)
+        return dict(APP_VERSION=pkg_resources.require('petri')[0].version)
+
+    @app.context_processor
+    def inject_index_title():
+        return dict(INDEX_TITLE='Short it!')
 
     return app
 
